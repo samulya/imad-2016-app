821b3e41287d10f64978d67b9cf155e4f0521a62
samulya <samulya114@gmail.com>
2016-11-08 10:00:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){ 
-    res.sendFile(path('article-one requested and will be served here'));
+    res.sendFile('article-one requested and will be served here');
     
 });
 
